90e451272689b0d173d20bc30ba282f30262d7f5
Jay <jay.patel@outdoorsy.co>
2021-07-21 17:22:53 -0700

    blah

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const random = require("random");
 
-const VERSION_FILENAME='./.git/refs/heads/main'
+const VERSION_FILENAME = "./.git/refs/heads/main";
 
-app.get('/', (req, res) => {
-  console.log('[hello-world] root handler called')
+app.get("/", (req, res) => {
+  console.log("[hello-world] root handler called");
+  const randomVar = random.bernoulli(0.5);
   res
-    .set('x-powered-by', 'cyclic.sh')
-    .send('<h1>Hello World what\'s up!!!!!! HI KAM!?!</h1>')
-    .end()
-})
+    .set("x-powered-by", "cyclic.sh")
+    .send(`<h1>Hello World what's up!!!!!! HI KAM!?! bernoulli random var ${randomVar}</h1>`)
+    .end();
+});
 
-app.use('*', (req,res) => {
-  console.log('[hello-world] Star handler called')
-  let version = 'unknown'
+app.use("*", (req, res) => {
+  console.log("[hello-world] Star handler called");
+  let version = "unknown";
   if (fs.existsSync(VERSION_FILENAME)) {
-    version = fs.readFileSync(VERSION_FILENAME).toString().substr(0,8)
+    version = fs.readFileSync(VERSION_FILENAME).toString().substr(0, 8);
   }
   res
-    .set('x-powered-by', 'cyclic.sh')
+    .set("x-powered-by", "cyclic.sh")
     .json({
       msg: "Not strickly part of the hello world but you get the picture.",
       version,
@@ -32,7 +34,7 @@ app.use('*', (req,res) => {
       headers: req.headers,
       cookies: req.cookies,
     })
-    .end()
-})
+    .end();
+});
 
-module.exports = app
+module.exports = app;
